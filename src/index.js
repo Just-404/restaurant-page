6bf043d87d79fa7927eb57941b24811fa25f8a73
Eddy <154628904+Just-404@users.noreply.github.com>
2025-01-02 17:29:46 -0400

    Add the tab-switching logic
    1. Create a function to remove every element in .content before
    adding the new ones.
    
    2. Add eventListeners to every button to generate their assigned content.
    
    3. Fix word in template where the last button said about instead of
    contact.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import createMainContent from "./home";
 import { createNodes } from "./menu";
 import createContact from "./contact";
 
-//createMainContent();
-//createNodes();
-createContact();
+let i = 0;
+const removeChildren = () => {
+    const mainDiv = document.querySelector(".content");
+    while(mainDiv.firstChild){
+        mainDiv.removeChild(mainDiv.lastChild);
+    }
+}
+
+const homeBtn = document.getElementById("homeBtn");
+const menuBtn = document.getElementById("menuBtn");
+const contactBtn = document.getElementById("contactBtn");
+
+createMainContent();
+
+homeBtn.addEventListener("click", () => {
+    removeChildren();
+    createMainContent();
+
+});
+menuBtn.addEventListener("click", () => {
+    removeChildren();
+    createNodes();
+    
+});
+contactBtn.addEventListener("click", () => {
+    removeChildren();
+    createContact();
+});
